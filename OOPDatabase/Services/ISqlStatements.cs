c3023805d753b91271cd782d2f91726494813e61
FZoltan40 <farkasz@kkszki.hu>
2025-09-22 08:08:55 +0200

    Interface and tablebooks class are edded.

---
+++ OOPDatabase/Services/ISqlStatements.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPDatabase.Services
+{
+    internal interface ISqlStatements
+    {
+    }
+}
