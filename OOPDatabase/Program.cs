1411b04d1992023321bbdffc80123c9fbbaf0e40
FZoltan40 <farkasz@kkszki.hu>
2025-09-22 10:14:32 +0200

    AddNweBook via clinet is  ready to use.

--- OOPDatabase/Program.cs
+++ OOPDatabase/Program.cs
@@ -16,14 +16,32 @@ namespace OOPDatabase
                 Console.WriteLine($"{book[0].Name} = {book[0].GetValue(item)}, {book[1].Name} = {book[1].GetValue(item)}");
             }*/
 
-            //Feladat2
+            /*Feladat2
             Console.Write("Kérem a rejord id-t: ");
 
             var item = sqlStatements.GetById(int.Parse(Console.ReadLine()));
 
             var book = item.GetType().GetProperties();
 
-            Console.WriteLine($"{book[1].Name} = {book[1].GetValue(item)}");
+            Console.WriteLine($"{book[1].Name} = {book[1].GetValue(item)}");*/
+
+            Console.Write("Kérem a könyv címét:");
+            string cim = Console.ReadLine();
+            Console.Write("Kérem a könyv szerzőjét:");
+            string szerzo = Console.ReadLine();
+            Console.Write("Kérem a könyv kiadási dátumát:");
+            string datum = Console.ReadLine();
+
+            var book = new
+            {
+                Title = cim,
+                Author = szerzo,
+                Release = datum
+
+            };
+
+            sqlStatements.AddNewRecord(book);
+
 
 
 
